a44948abb3f17f77a37dd17a1f2b58ffa77dee01
Cosmodash <theo.github@bedafamily.com>
2018-08-24 18:32:35 -0700

    change build script

--- mingw-launch.cmd
+++ mingw-launch.cmd
@@ -1,3 +1,3 @@
 @echo off
-g++ -o bin\game.exe src\*.cpp -Link SDL2.dll -I"Z:\c++lib\SDL2\include" -I"Z:\c++lib\SDL2\include\SDL2"
+g++ -o bin\game.exe src\*.cpp -Link SDL2.dll -I"Z:\c++lib\SDL2\include\SDL2"
 bin\game
