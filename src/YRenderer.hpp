78800d63aa04c97283f60710d0d08a8128c31664
Joe Beda <joe.github@bedafamily.com>
2019-06-06 21:36:18 -0700

    Sketch out how push/pop on renderer will work
    Signed-off-by: Joe Beda <joe.github@bedafamily.com>

--- src/YRenderer.hpp
+++ src/YRenderer.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <SDL.h>
+#include <vector>
+#include "Vector2D.hpp"
 
 class YRenderer
 {
 public:
-	YRenderer() : renderer(NULL) {}
+	YRenderer() : renderer(NULL), origin(0,0) {}
 	~YRenderer() { Clean(); }
 
 	void Clean() { SDL_DestroyRenderer(renderer); renderer = NULL; }
@@ -17,6 +19,7 @@ public:
 	int Clear() { return  SDL_RenderClear(renderer); }
 	void Present() { SDL_RenderPresent(renderer); }
 	int Copy(SDL_Texture* t, const SDL_Rect* srcrect, const SDL_Rect* dstrect) {
+		// adjust based on origin
 		return SDL_RenderCopy(renderer, t, srcrect, dstrect);
 	}
 	int CopyEx(SDL_Texture* texture,
@@ -25,10 +28,12 @@ public:
 			const double angle,
 			const SDL_Point* center,
 			const SDL_RendererFlip flip) {
+		// adjust based on origin
 		return SDL_RenderCopyEx(renderer, texture, srcrect, dstrect, angle, center, flip);
 	}
 
 	int FillRect(const SDL_Rect* rect) {
+		// adjust based on origin
 		return SDL_RenderFillRect(renderer, rect);
 	}
 
@@ -40,7 +45,21 @@ public:
 		return SDL_SetRenderDrawColor(renderer, r, g, b, a);
 	}
 
+	void PushOrigin(Vector2f o) {
+		// Take origin and push it on origins
+		// Add origin to o
+		// set origin to o
+	}
+	void PopOrigin() {
+		// remove most recent from origins
+		// set origin to that
+		// error/assert if origins is empty
+	}
+
 private:
 	SDL_Renderer* renderer;
+
+	Vector2f origin;
+	std::vector<Vector2f> origins;
 };
 
